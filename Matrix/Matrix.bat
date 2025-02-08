dea5d26ae2478b529d1b7d3a09f6dbaabc913c32
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-02-08 15:06:44 +0100

    Create Matrix.bat

---
+++ Matrix/Matrix.bat
@@ -0,0 +1,7 @@
+@echo off
+
+color a ::change the color to green
+
+a: ::function
+echo %%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%%%%random%% :: add random numbers to cmd
+goto a :: start the function again
