510891f54b2b49da91b804387cdac0316ff67757
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-02-08 10:57:32 +0100

    Create Caesar_Python_En.py

---
+++ Caesar/Caesar_Python_En.py
@@ -0,0 +1,23 @@
+# Created by Gabypotter08 in Python 3.7
+# Coding utf-8
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+min=[]
+
+phrase=input("Sentence : ")
+phrase_caesar=''
+lettre_indiciel=''
+phrase_indiciel=''
+
+deplacement=input("How much letters is the mooving ?")
+deplacement=int(deplacement)
+
+for i in range(len(phrase)):
+    if phrase[i]==' ':
+        phrase_caesar=phrase_caesar+' '
+    else:
+        phrase_caesar=phrase_caesar + alphabet[alphabet.index(phrase[i])+deplacement]
+
+print(phrase_caesar)
