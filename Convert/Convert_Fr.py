d31d97766b1a6d2990ae018bbd03a9604e1d1e2a
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-02-10 14:56:24 +0100

    Update Convert_Fr.py

--- Convert/Convert_Fr.py
+++ Convert/Convert_Fr.py
@@ -1,29 +1,30 @@
-# Created by Gabypotter08 in Python 3.7
+# Created by Gabypotter08, the 10/02/2025 in Python 3.7
 # Coding utf-8
 
-def celsius(valeur_fahrenheit) -> float:
+def celsius(valeur_fahrenheit:int=0) -> float:
     """
     convertir fahrenheit en celsius
     """
     valeur_celsius=(valeur_fahrenheit-32)*5/9
     return valeur_celsius
 
-def fahrenheit(valeur_celsius)->float:
+def fahrenheit(valeur_celsius:int=0)->float:
     """
     convertir celsius en fahrenheit
     """
     valeur_fahrenheit=(valeur_celsius* 9/5) + 32 
     return valeur_fahrenheit
 
-def miles(valeur_km)-> float:
+def miles(valeur_km:int=0)-> float:
     """
     convertir kilomètres en miles
     """
     valeur_miles=valeur_km/1,609
     return valeur_miles
 
-def km(valeur_miles)-> float:
+def km(valeur_miles:int=0)-> float:
     """
     convertir miles en kilomètres
     """
     valeur_km=valeur_miles*1,609
+    return valeur_km
