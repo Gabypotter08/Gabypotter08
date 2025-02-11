e4823b9327757bda6af1ad138f1eecd345ec1396
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-02-11 09:44:26 +0100

    Create Function_Caesar_Fr.py

---
+++ Caesar/Function_Caesar_Fr.py
@@ -0,0 +1,17 @@
+# Created by Gabypotter08, the 11/02/2025 in Python 3.7
+# Coding utf-8
+
+def caesar(sentance:str, deplacement:int=1):
+  """
+  crypte la phrase avec un déplacement : deplacement
+  """
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  min=[]
+  phrase_caesar=''
+
+  for i in range(len(phrase)):
+      if phrase[i]==' ':
+          phrase_caesar=phrase_caesar+' '
+      else:
+          phrase_caesar=phrase_caesar + alphabet[alphabet.index(phrase[i])+deplacement]
+  return phrase_caesar
