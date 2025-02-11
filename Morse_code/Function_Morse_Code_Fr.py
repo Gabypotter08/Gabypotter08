d06a50a4f9e8a305f825244fca9846e1e1420a2e
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-02-11 10:09:59 +0100

    Update Function_Morse_Code_Fr.py

--- Morse_code/Function_Morse_Code_Fr.py
+++ Morse_code/Function_Morse_Code_Fr.py
@@ -111,125 +111,125 @@ def crypteur(word:str):
     return wordcrypter
 
     
-    def decrypteur():
-        """
-        Décrypte le mot ou la phrase du morse vers le lisible
-        """
-        word=[]
-        i=0
-        while i < lengh:
-            if wordcrypter[i:i+3]==['.', '_', ' ']:
-                word.append('a')
-                i=i+3
-            elif wordcrypter[i:i+5]==['_', '.', '.', '.', ' ']:
-                word.append('b')
-                i=i+5
-            elif wordcrypter[i:i+5]==['_', '.', '_', '.', ' ']:
-                word.append('c')
-                i=i+5
-            elif wordcrypter[i:i+4]==['_', '.', '.', ' ']:
-                word.append('d')
-                i=i+4
-            elif wordcrypter[i:i+2]==['.', ' ']:
-                word.append('e')
-                i=i+2
-            elif wordcrypter[i:i+5]==['.', '.', '_', '.', ' ']:
-                word.append('f')
-                i=i+5
-            elif wordcrypter[i:i+4]==['_', '_', '.', ' ']:
-                word.append('g')
-                i=i+4
-            elif wordcrypter[i:i+5]==['.', '.', '.', '.', ' ']:
-                word.append('h')
-                i=i+5
-            elif wordcrypter[i:i+3]==['.', '.', ' ']:
-                word.append('i')
-                i=i+3
-            elif wordcrypter[i:i+5]==['.', '_', '_', '_', ' ']:
-                word.append('j')
-                i=i+5
-            elif wordcrypter[i:i+4]==['_', '.', '_', ' ']:
-                word.append('k')
-                i=i+4
-            elif wordcrypter[i:i+5]==['.', '_', '.', '.', ' ']:
-                word.append('l')
-                i=i+5
-            elif wordcrypter[i:i+3]==['_', '_', ' ']:
-                word.append('m')
-                i=i+3
-            elif wordcrypter[i:i+3]==['_', '.', ' ']:
-                word.append('n')
-                i=i+3
-            elif wordcrypter[i:i+4]==['_', '_', '_', ' ']:
-                word.append('o')
-                i=i+4
-            elif wordcrypter[i:i+5]==['.', '_', '_', '.', ' ']:
-                word.append('p')
-                i=i+5
-            elif wordcrypter[i:i+5]==['_', '_', '.', '_', ' ']:
-                word.append('q')
-                i=i+5
-            elif wordcrypter[i:i+4]==['.', '_', '.', ' ']:
-                word.append('r')
-                i=i+4
-            elif wordcrypter[i:i+4]==['.', '.', '.', ' ']:
-                word.append('s')
-                i=i+4
-            elif wordcrypter[i:i+2]==['_', ' ']:
-                word.append('t')
-                i=i+2
-            elif wordcrypter[i:i+4]==['.', '.', '_', ' ']:
-                word.append('u')
-                i=i+4
-            elif wordcrypter[i:i+5]==['.', '.', '.', '_', ' ']:
-                word.append('v')
-                i=i+5
-            elif wordcrypter[i:i+4]==['.', '_', '_', ' ']:
-                word.append('w')
-                i=i+4
-            elif wordcrypter[i:i+5]==['_', '.', '.', '_', ' ']:
-                word.append('x')
-                i=i+5
-            elif wordcrypter[i:i+5]==['_', '.', '_', '_', ' ']:
-                word.append('y')
-                i=i+5
-            elif wordcrypter[i:i+5]==['_', '_', '.', '.', ' ']:
-                word.append('z')
-                i=i+5
-            elif wordcrypter[i:i+2]==[' ', ' ']:
-                word.append(' ')
-                i=i+2
-            elif wordcrypter[i:i+6]==['_', '_', '_', '_', '_', ' ']:
-                word.append('0')
-                i=i+6
-            elif wordcrypter[i:i+6]==['.', '_', '_', '_', '_', ' ']:
-                word.append('1')
-                i=i+6
-            elif wordcrypter[i:i+6]==['.', '.', '_', '_', '_', ' ']:
-                word.append('2')
-                i=i+6
-            elif wordcrypter[i:i+6]==['.', '.', '.', '_', '_', ' ']:
-                word.append('3')
-                i=i+6
-            elif wordcrypter[i:i+6]==['.', '.', '.', '.', '_', ' ']:
-                word.append('4')
-                i=i+6
-            elif wordcrypter[i:i+6]==['.', '.', '.', '.', '.', ' ']:
-                word.append('5')
-                i=i+6
-            elif wordcrypter[i:i+6]==['_', '.', '.', '.', '.', ' ']:
-                word.append('6')
-                i=i+6
-            elif wordcrypter[i:i+6]==['_', '_', '.', '.', '.', ' ']:
-                word.append('7')
-                i=i+6
-            elif wordcrypter[i:i+6]==['_', '_', '_', '.', '.', ' ']:
-                word.append('8')
-                i=i+6
-            elif wordcrypter[i:i+6]==['_', '_', '_', '_', '.', ' ']:
-                word.append('9')
-                i=i+6
-            else:
-                print('erreur tour',i)
-                i=i+1
-        return word
+def decrypteur():
+    """
+    Décrypte le mot ou la phrase du morse vers le lisible
+    """
+    word=[]
+    i=0
+    while i < lengh:
+        if wordcrypter[i:i+3]==['.', '_', ' ']:
+            word.append('a')
+            i=i+3
+        elif wordcrypter[i:i+5]==['_', '.', '.', '.', ' ']:
+            word.append('b')
+            i=i+5
+        elif wordcrypter[i:i+5]==['_', '.', '_', '.', ' ']:
+            word.append('c')
+            i=i+5
+        elif wordcrypter[i:i+4]==['_', '.', '.', ' ']:
+            word.append('d')
+            i=i+4
+        elif wordcrypter[i:i+2]==['.', ' ']:
+            word.append('e')
+            i=i+2
+        elif wordcrypter[i:i+5]==['.', '.', '_', '.', ' ']:
+            word.append('f')
+            i=i+5
+        elif wordcrypter[i:i+4]==['_', '_', '.', ' ']:
+            word.append('g')
+            i=i+4
+        elif wordcrypter[i:i+5]==['.', '.', '.', '.', ' ']:
+            word.append('h')
+            i=i+5
+        elif wordcrypter[i:i+3]==['.', '.', ' ']:
+            word.append('i')
+            i=i+3
+        elif wordcrypter[i:i+5]==['.', '_', '_', '_', ' ']:
+            word.append('j')
+            i=i+5
+        elif wordcrypter[i:i+4]==['_', '.', '_', ' ']:
+            word.append('k')
+            i=i+4
+        elif wordcrypter[i:i+5]==['.', '_', '.', '.', ' ']:
+            word.append('l')
+            i=i+5
+        elif wordcrypter[i:i+3]==['_', '_', ' ']:
+            word.append('m')
+            i=i+3
+        elif wordcrypter[i:i+3]==['_', '.', ' ']:
+            word.append('n')
+            i=i+3
+        elif wordcrypter[i:i+4]==['_', '_', '_', ' ']:
+            word.append('o')
+            i=i+4
+        elif wordcrypter[i:i+5]==['.', '_', '_', '.', ' ']:
+            word.append('p')
+            i=i+5
+        elif wordcrypter[i:i+5]==['_', '_', '.', '_', ' ']:
+            word.append('q')
+            i=i+5
+        elif wordcrypter[i:i+4]==['.', '_', '.', ' ']:
+            word.append('r')
+            i=i+4
+        elif wordcrypter[i:i+4]==['.', '.', '.', ' ']:
+            word.append('s')
+            i=i+4
+        elif wordcrypter[i:i+2]==['_', ' ']:
+            word.append('t')
+            i=i+2
+        elif wordcrypter[i:i+4]==['.', '.', '_', ' ']:
+            word.append('u')
+            i=i+4
+        elif wordcrypter[i:i+5]==['.', '.', '.', '_', ' ']:
+            word.append('v')
+            i=i+5
+        elif wordcrypter[i:i+4]==['.', '_', '_', ' ']:
+            word.append('w')
+            i=i+4
+        elif wordcrypter[i:i+5]==['_', '.', '.', '_', ' ']:
+            word.append('x')
+            i=i+5
+        elif wordcrypter[i:i+5]==['_', '.', '_', '_', ' ']:
+            word.append('y')
+            i=i+5
+        elif wordcrypter[i:i+5]==['_', '_', '.', '.', ' ']:
+            word.append('z')
+            i=i+5
+        elif wordcrypter[i:i+2]==[' ', ' ']:
+            word.append(' ')
+            i=i+2
+        elif wordcrypter[i:i+6]==['_', '_', '_', '_', '_', ' ']:
+            word.append('0')
+            i=i+6
+        elif wordcrypter[i:i+6]==['.', '_', '_', '_', '_', ' ']:
+            word.append('1')
+            i=i+6
+        elif wordcrypter[i:i+6]==['.', '.', '_', '_', '_', ' ']:
+            word.append('2')
+            i=i+6
+        elif wordcrypter[i:i+6]==['.', '.', '.', '_', '_', ' ']:
+            word.append('3')
+            i=i+6
+        elif wordcrypter[i:i+6]==['.', '.', '.', '.', '_', ' ']:
+            word.append('4')
+            i=i+6
+        elif wordcrypter[i:i+6]==['.', '.', '.', '.', '.', ' ']:
+            word.append('5')
+            i=i+6
+        elif wordcrypter[i:i+6]==['_', '.', '.', '.', '.', ' ']:
+            word.append('6')
+            i=i+6
+        elif wordcrypter[i:i+6]==['_', '_', '.', '.', '.', ' ']:
+            word.append('7')
+            i=i+6
+        elif wordcrypter[i:i+6]==['_', '_', '_', '.', '.', ' ']:
+            word.append('8')
+            i=i+6
+        elif wordcrypter[i:i+6]==['_', '_', '_', '_', '.', ' ']:
+            word.append('9')
+            i=i+6
+        else:
+            print('erreur tour',i)
+            i=i+1
+    return word
