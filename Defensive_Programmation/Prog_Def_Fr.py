dfdc9c1447ad8ff9853c32fb6e550581bd63a1c0
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-02-12 10:32:18 +0100

    Update Prog_Def_Fr.py

--- Defensive_Programmation/Prog_Def_Fr.py
+++ Defensive_Programmation/Prog_Def_Fr.py
@@ -7,38 +7,6 @@ Programmation défensive et bonnes pratique en python
 Exemples avec : vérifier une liste triée, cherche max/min, calcul moyenne
 """
 
-
-"""
-#exemple utilisation assertions et mise en page d'une fonction
-def ma_fonction(a: str='a', b: str='b', c: int=3, d:bool=False)->str:
-    assert type(a)==str and type(b)==str, "Il faut passer un str" #Assertion, suivi d'une expression logique évaluer à True suivi d'un msg d'erreur si évaluée à False
-    assert type(c)==int, "Il faut passer un int"
-    assert type(d)==bool, "Il faut passer un booléen"
-    if d== True:
-        resultat=a+b
-    else:
-        resultat=a*c
-    return resultat
-"""
-
-#Croissant
-
-"""
-def tri_ok(liste:list)->bool:
-    #""
-    Renvoie True si la liste est triée par ordre croissant
-    #""
-    for i in range(len(liste)-1):
-        if liste[i+1]<liste[i]:
-            return False
-    return True
-
-assert tri_ok([1,4,5,6,12,74]) == True
-assert tri_ok([1,4,5,6,12,-4]) == False
-"""
-
-#Croissant ou décroissant
-
 def tri_ok(liste:list, c:bool=True)->bool:
     #DOCSTRINGS
     """
@@ -111,6 +79,101 @@ def extremum(liste:list, m:bool=True)->float:
 
 
 
+def moyenne(liste:list)->float:
+    """
+    Renvoie la moyenne des valeurs d'une liste
+    """
+    assert len(liste)>0, "La liste est vide"
+    total=0
+    for i in range(len(liste)):
+        try:
+            liste[i] =float(liste[i]) #essaie de convertir la valeur en float
+        except: #except généraliste
+            continue #si impossible, on passe à la valeur suivante dans la liste
+        total+= liste[i]
+    return total/len(liste)
+
+def moyenne2(liste:list)->float:
+    return sum(liste)/len(liste)
+
+def moyenne_coef(liste:list)->float:
+    """
+    Renvoie la moyenne coefficientée des valeursd e la liste
+    valeur de la liste = paire note, coef
+    """
+    assert len(liste)>0, "La liste est vide"
+    total=0
+    coef=0
+    for i in range(len(liste)):
+        try:
+            liste[i][0] = float(liste[i][0]) #essaie de convertir la valeur en float
+            liste[i][1] = float(liste[i][1])
+        except:
+            continue
+        total += liste[i][0] * liste[i][1]
+        coef += liste[i][1]
+    return total/coef
+
+
+#façon alternative de gérer les boucles for spécifique à Python
+def extremum2(liste:list, m:bool=True)->float:
+    """
+    Renvoie la valeur maxi ou mini d'une liste de valeurs numériques
+    Si m==True, rnvoie la maxi si m==False, renvoi le mini
+    """
+    assert len(liste)>0, "La liste est vide"
+    retour = liste[0]
+    for val in list:
+        try:
+            val = float(val) #essaie de convertir la valeur en float
+        except: #except généraliste
+            continue #si impossible, on passe à la valeur suivante dans la liste
+
+        if m: #On cherche le maximum
+            if val > retour:
+                    retour = val
+        else: #On cherche le minimum
+            if val < retour:
+                    retour = val
+    return retour
+
+def moyenne3(liste:list)->float:
+    """
+    Renvoie la moyenne des valeurs d'une liste
+    """
+    assert len(liste)>0, "La liste est vide"
+    total=0
+    for note in liste:
+        try:
+            note=float(note)
+        except:
+            continue
+        total+= note
+    return total/len(liste)
+
+def moyenne_coef2(liste:list)->float:
+    """
+    Renvoie la moyenne coefficientée des valeursd e la liste
+    valeur de la liste = paire note, coef
+    """
+    assert len(liste)>0, "La liste est vide"
+    total=0
+    coef=0
+    for note,cf in liste:
+        try:
+            note= float(note)
+            cf= float(cf)
+        except:
+            continue
+        total += note*cf
+        coef += cf
+    return total/coef
+
+
+
+
+
+
 
 
 assert tri_ok([1,4,5,6,12,74]) == True
@@ -119,9 +182,18 @@ assert tri_ok([1,4,5,6,12,74],c=False) == False
 assert tri_ok([11,4,-5,-6,-12,-74],c=False) == True
 
 assert maximum([1478, 2, 5, 4, 78, "-98", 511, 'er', 1478]) == 1478
+#Compare le résultat de nos fonctions avec celui des fonctions natives
+assert maximum([1478, 2, 5, 4, 78, "-98", 511, 'er', -1478]) == max([1478, 2, 5, 4, 78, 511, 1478])
 
 assert minimum([1478, 2, 5, 4, 78, "-98", 511, 'er', -1478]) == -1478
+assert minimum([1478, 2, 5, 4, 78, "-98", 511, 'er', -1478]) == min([1478, 2, 5, 4, 78, 511, -1478])
 
 assert extremum([1478, 2, 5, 4, 78, "-98", 511, 'er', -1478]) == 1478
 assert extremum([1478, 2, 5, 4, 78, "-98", 511, 'er', -1478], m=False) == -1478
 
+assert moyenne([1,2,3,5,8,45])==mean([1,2,3,5,8,45])
+assert moyenne([1,2,3,5,8,45])==moyenne2([1,2,3,5,8,45])
+assert moyenne_coef([[12,4],[10,1],[14,3]]) == 12.5
+assert moyenne_coef([[12,4],['abs',5],[14,4]]) == moyenne_coef([[12,4],[14,4]])
+assert moyenne_coef2([[12,4],['abs',5],[14,4]]) == moyenne_coef2([[12,4],[14,4]])
+assert moyenne_coef2([[12,4],[10,1],[14,3]]) == 12.5
