8dbe621f4b25458a1e0464dd785793026a4a6e4a
Gabypotter08 <155171103+Gabypotter08@users.noreply.github.com>
2025-04-16 22:01:30 +0200

    Update Convert_En.py

--- Convert/Convert_En.py
+++ Convert/Convert_En.py
@@ -1,30 +1,46 @@
 # Created by Gabypotter08, the 10/02/2025 in Python 3.7
 # Coding utf-8
 
-def celsius(valeur_fahrenheit:int=0) -> float:
+def celsius(val_fahrenheit:int=0) -> float:
     """
     Convert fahrenheit in celsius
     """
-    valeur_celsius=(valeur_fahrenheit-32)*5/9
-    return valeur_celsius
+    val_celsius=(val_fahrenheit-32)*5/9
+    return val_celsius
 
-def fahrenheit(valeur_celsius:int=0)->float:
+def fahrenheit(val_celsius:int=0)->float:
     """
-    convert celsius en fahrenheit
+    convert celsius to fahrenheit
     """
-    valeur_fahrenheit=(valeur_celsius* 9/5) + 32 
-    return valeur_fahrenheit
+    val_fahrenheit=(val_celsius* 9/5) + 32 
+    return val_fahrenheit
 
-def miles(valeur_km:int=0)-> float:
-    """
-    convert kilometres in miles
-    """
-    valeur_miles=valeur_km/1,609
-    return valeur_miles
 
-def km(valeur_miles:int=0)-> float:
+def miles(val_km:int=0)-> float:
     """
-    convert miles en kilometres
+    convert kilometres to miles
     """
-    valeur_km=valeur_miles*1,609
-    return valeur_km
+    val_miles=val_km/1,609
+    return val_miles
+
+def km(val_miles:int=0)-> float:
+    """
+    convert miles to kilometres
+    """
+    val_km=val_miles*1,609
+    return val_km
+
+
+def pound(val_kg:int=0)-> float:
+    """
+    convert kilograms to pound
+    """
+    val_pound=val_kg/2,205
+    return val_pound
+    
+def kg(val_pound:int=0)-> float:
+    """
+    convert pound to kilograms
+    """
+    val_kg=val_pound*2,205
+    return val_kg
